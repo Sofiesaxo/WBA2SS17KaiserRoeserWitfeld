a2de99887699bbf7200b7bd5659fc7a12f7b16b5
Evillienchen <sonja_yvonne@web.de>
2017-05-05 21:13:43 +0200

    Aufgabe 1

---
+++ sonja_workshop1/app.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+
+fs.readFile(__dirname+"/staedte.json", function(err, data) {
+  if(err){
+    console.log(err);
+    return;
+  }
+  else{
+    data=JSON.parse(data);
+    for(var i = 0;i<data.cities.length;i++){
+      console.log("name: "+data.cities[i].name);
+      console.log("country: "+data.cities[i].country);
+      console.log("population: "+data.cities[i].population);
+      console.log("- - - - - - - - - - - - - - - -");
+    }
+  }
+
+   });
