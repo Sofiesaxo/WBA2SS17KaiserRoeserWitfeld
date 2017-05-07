d1ea9ba4c94c6e1c3a79edccc3112e9c91fcba09
Leah Röser <leah.roeser@hotmail.de>
2017-05-07 15:29:51 +0200

    Aufgabe2

--- Leah_workshop1/App.js
+++ Leah_workshop1/App.js
@@ -1,4 +1,6 @@
 var fs = require('fs');
+var chalk = require('chalk');
+
 fs.readFile(__dirname+"Staedte.json", function(err, data) { 
     if (err){
         konsole.log(err);
@@ -7,9 +9,9 @@ fs.readFile(__dirname+"Staedte.json", function(err, data) {
     else(data= JSON.parse(data));
          }
         for(var i=0;i<data.cities.length;i++){
-        konsole.log("name: "+data.cities[i].name);
-        konsole.log("country"+data.cities[i].country);
-        konsole.log("population"+data.cities[i].population);
+        konsole.log("name: "+chalk.pink(data.cities[i].name));
+        konsole.log("country"+chalk.green(data.cities[i].country));
+        konsole.log("population"+chalk.blue(data.cities[i].population));
         konsole.log("------------------");
     }
 
