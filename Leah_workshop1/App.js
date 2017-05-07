5f0eb826a85d7c9f8151c6ad6353da361a110492
Leah Röser <leah.roeser@hotmail.de>
2017-05-07 15:51:57 +0200

    Aufgabe3

--- Leah_workshop1/App.js
+++ Leah_workshop1/App.js
@@ -1,18 +1,31 @@
 var fs = require('fs');
 var chalk = require('chalk');
 
-fs.readFile(__dirname+"Staedte.json", function(err, data) { 
+fs.readFile(__dirname+"/staedte.json", function(err, data) { 
     if (err){
-        konsole.log(err);
+        console.log(err);
         return;
     }
     else(data= JSON.parse(data));
-         }
+         
+            data.cities.sort(function(a,b){
+    if(a.population > b.population){
+        return 1;
+    }
+    if (a.population < b.population){
+        return -1;
+    }
+    return 0;
+    
+});
+fs.writeFileSync(__dirname+"/staedte_sortiert.json", JSON.stringify(data));
+
         for(var i=0;i<data.cities.length;i++){
-        konsole.log("name: "+chalk.pink(data.cities[i].name));
-        konsole.log("country"+chalk.green(data.cities[i].country));
-        konsole.log("population"+chalk.blue(data.cities[i].population));
-        konsole.log("------------------");
+        console.log("name: "+chalk.red(data.cities[i].name));
+        console.log("country"+chalk.green(data.cities[i].country));
+        console.log("population"+chalk.blue(data.cities[i].population));
+        console.log("------------------");
     }
 
+});
                                                            
